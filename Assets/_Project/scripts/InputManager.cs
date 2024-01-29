c1deffba49522389f64976b41e78bfa6047ed3fe
fgalcodes <fabian.galindo.7e6@itb.cat>
2024-01-29 18:59:47 +0100

    movement controller

--- Assets/_Project/scripts/InputManager.cs
+++ Assets/_Project/scripts/InputManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+
+    PlayerInputs inputs;
+    Animator animator;
+
+    private void Awake()
+    {
+        inputs = new PlayerInputs();
+        animator = GetComponent<Animator>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,32 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(GetPlayerMovement().magnitude != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+
+        }
+
+        animator.SetFloat("moveX", GetPlayerMovement().x);
+        animator.SetFloat("moveZ", GetPlayerMovement().y);
+    }
+
+    private void OnEnable()
+    {
+        inputs.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputs.Disable();
+    }
+
+    public Vector2 GetPlayerMovement()
+    {
+        return inputs.Player.Walk.ReadValue<Vector2>();
     }
 }

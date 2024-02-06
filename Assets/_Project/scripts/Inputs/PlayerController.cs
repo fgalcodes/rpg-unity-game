bbeff30f7627be2ecd0eaf71f08c021cfbdb81c2
fgalcodes <fabian.galindo.7e6@itb.cat>
2024-02-06 19:13:59 +0100

    controller fix

--- Assets/_Project/scripts/Inputs/PlayerController.cs
+++ Assets/_Project/scripts/Inputs/PlayerController.cs
@@ -2,33 +2,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("References")] [SerializeField]
-    CharacterController controller;
-
+    [Header("References")]
+    [SerializeField] CharacterController controller;
     [SerializeField] Animator animator;
     [SerializeField] InputReader input;
 
+    [SerializeField] float playerSpeed = 2.0f;
+    float currentSpeed;
+    float velocity;
 
-    [Header("Settings")] [SerializeField] float playerSpeed = 1.0f;
-
-    private float currentSpeed;
+    const float ZeroF = 0.0f;
 
     // Animator parameters
     private static readonly int Speed = Animator.StringToHash("Speed");
 
-    private const float ZeroF = 0.0f;
-
-    private void Awake()
-    {
-        // controller = gameObject.GetComponent<CharacterController>();
-        // animator = gameObject.GetComponent<Animator>();
-        // input = gameObject.GetComponent<InputReader>();
-    }
-
     void Update()
     {
-        Vector3 move = new Vector3(input.Direction.x, 0, input.Direction.y).normalized;
-        controller.Move(move * (Time.deltaTime * playerSpeed));
-        animator.SetFloat(Speed, playerSpeed);
+        Vector3 move = new Vector3(input.Direction.x, 0, input.Direction.y);
+
+        if (move.magnitude > ZeroF)
+        {
+            controller.Move(move * (playerSpeed * Time.deltaTime));
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, move.magnitude, ref velocity, .2f);
+
+        } else
+        {
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, ZeroF, ref velocity, .2f);
+        }
+
+        if (currentSpeed < 0 || currentSpeed > 1)
+        {
+            currentSpeed = ZeroF;
+        }
+
+        animator.SetFloat(Speed, currentSpeed);
     }
 }
